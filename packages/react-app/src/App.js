18c4d101b32b4de1836e1b360d64b184a75bcc0c
Mitrasish Mukherjee <mmitrasish97@gmail.com>
2023-01-18 23:04:39 +0530

    Update router to hash router for spheron deployment

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import CreateNFT from "./pages/CreateNFT";
 import OwnerNFTs from "./pages/OwnedNFTs";
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route path="/create" element={<CreateNFT />} />
         <Route path="/ownednfts" element={<OwnerNFTs />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
--- packages/react-app/src/components/Navbar/index.jsx
+++ packages/react-app/src/components/Navbar/index.jsx
@@ -1,13 +1,11 @@
 import { ConnectKitButton } from "connectkit";
-import { NavLink, Router,LinkProps } from "react-router-dom";
+import { Router } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-between px-10 bg-slate-50 shadow-xl py-4 text-xl w-full">
       <h2 className="w-full">MarketPlace</h2>
       <div className="flex justify-evenly w-full">
-        <button onClick={()=>Router.push('/')}>
-          Home
-        </button>
+        <button onClick={() => Router.push("/")}>Home</button>
         <h2>Owned</h2>
         <h2>Mint NFT</h2>
       </div>
